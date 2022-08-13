1edd742ddeef19badea30a7f0eea782088d93a64
Рома <57133971+nazmiev@users.noreply.github.com>
2022-08-13 12:35:53 +0700

    start adding redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from './redux/slices/filterSlice'
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./Pages/Home";
@@ -10,11 +12,27 @@ export const SearchContext = React.createContext("");
 
 function App() {
   const [searchValue, setSearchValue] = React.useState("");
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-        <Header/>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -22,7 +40,7 @@ function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   );
 }
